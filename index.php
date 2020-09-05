6942455d4d94cd99a6c50db67a448be9d6bb2609
aakarshan1 <aakarshan7raj@gmail.com>
2020-09-05 19:18:07 +0530

    Original State

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+
+$connection = mysqli_connect("localhost","padmin","password","darknet");
+if(!$connection){ echo "Ereror";};
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>zhn32zasffh5t6rj.onion</title>
+</head>
+<body>
+	
+	<div id = "me" style="color:red">
+	
+</div>
+
+<form method = "post" action = "index.php" id="enter"> 
+<h2 id="gret">Enter the blackBox</h2>
+<textarea rows="20" cols="30" name="data">
+	
+</textarea>
+<br>
+<input type="submit" name="Enter" value="Enter" id = "butt">
+</form>
+	
+</body>
+</html>
+
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ -->
+
+ <?php if(isset($_POST['Enter'])){
+
+ $data = $_POST['data'];
+ $query = "INSERT INTO details(ip,message) VALUES('$_SERVER[REMOTE_ADDR]','$data')";
+ $result = mysqli_query($connection,$query);
+ if(!$result){ echo "Error";}
+}
+?>
